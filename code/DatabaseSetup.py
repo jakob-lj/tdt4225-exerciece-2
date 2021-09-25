731ff1bc122c7a541e49d5a5bc39f4ad45fe8bad
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-09-25 13:49:40 +0200

    feat: initialise setup

--- code/DatabaseSetup.py
+++ code/DatabaseSetup.py
@@ -3,4 +3,16 @@ from tabulate import tabulate
 
 
 class DatabaseSetup:
-    pass
+    def __init__(self, logger, cursor):
+        self.logger = logger
+        self.cursor = cursor
+
+    def setup(self):
+        self.logger.debug("Setting up database")
+
+        self.logger.debug("Getting tables already existing")
+        print(self.cursor.execute("SHOW TABLES"))
+
+       # existingDatabases =
+
+       # self.logger.debug(existingDatabases)
--- code/main.py
+++ code/main.py
@@ -1,21 +1,28 @@
 #from .DatabaseSetup import DatabaseSetup
 #from code.DbConenctor import DatabaseConnector
 from DbConnector import DbConnector
+from DatabaseSetup import DatabaseSetup
 from Logger import Logger, LogLevel
 
 logger = Logger(LogLevel.DEBUG)
 
 
-def main():
+def main(dbConnector, databaseSetup):
 
     logger.info("Welcome to TDT4225 exercice 2")
 
-    dbConnector = DbConnector(
-        HOST="localhost", PASSWORD="password", USER="root", DATABASE="tdt4225", logger=logger)
+    databaseSetup.setup()
 
     dbConnector.close_connection()
 
 
 if __name__ == '__main__':
     logger.info("Hello")
-    main()
+
+    dbConnector = DbConnector(
+        HOST="localhost", PASSWORD="password", USER="root", DATABASE="tdt4225", logger=logger)
+
+    databaseSetup = DatabaseSetup(
+        cursor=dbConnector.cursor, logger=logger)
+
+    main(dbConnector, databaseSetup)
